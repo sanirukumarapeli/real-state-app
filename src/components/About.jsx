a321b462d85a93c12b5d8cd87d50a1777d52e01e
Saniru Dewmina <sanirukumarapeli@gmail.com>
2025-11-28 19:24:15 +0530

    feat: add About component and update Navbar links for improved navigation

---
+++ src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+function About() {
+  return (
+    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
+        <p className='text-gray-500 max-w-80 text-center mb-8 mx-auto'>Passionate about delivering quality and excellence in every project we undertake.</p>
+        <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
+            <img src={assets.brand_img} className="w-full sm:w-1/2 max-w-lg" alt="Our Brand" />
+            <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
+                <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl pr-28'>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>10+</p>
+                        <p>Years of Experience</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>12+</p>
+                        <p>Projects Completed</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>20+</p>
+                        <p>Mn. Sq. Ft. Delivered</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>25+</p>
+                        <p>Ongoing Projects</p>
+                    </div>
+                </div>
+                <p className='my-10 max-w-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus consequatur odio, doloremque magnam nisi reiciendis eius natus debitis minima qui sint laborum nemo fuga enim praesentium libero vero deleniti. Quidem.</p>
+                <button className='bg-blue-600 text-white px-8 py-2 rounded'>Learn More</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
